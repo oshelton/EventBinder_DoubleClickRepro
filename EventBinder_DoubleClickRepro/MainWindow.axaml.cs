da0735f54d8a3c16cc4425ee4e403cb4a223296f
Owen Shelton <jowenshelton@gmail.com>
2023-03-28 20:01:16 -0400

    Create example app.

---
+++ EventBinder_DoubleClickRepro/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace EventBinder_DoubleClickRepro;
+
+public sealed partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EventBinder_DoubleClickRepro/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBinder_DoubleClickRepro;
+
+public sealed class MainWindowViewModel : ReactiveObject
+{
+	public void ButtonClicked()
+	{
+		Counter++;
+	}
+
+	private int _counter = 0;
+	public int Counter
+	{
+		get => _counter;
+		set => this.RaiseAndSetIfChanged(ref _counter, value);
+	}
+}
